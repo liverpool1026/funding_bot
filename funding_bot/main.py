d2994b5855cbce8d43a477fd6b1ba20c6d0716bb
Kevin Hwa <liverpoo1026.bne@gmail.com>
2020-05-18 19:02:07 +1000

    Logging add timestamp

--- funding_bot/main.py
+++ funding_bot/main.py
@@ -4,7 +4,11 @@ from funding_bot.bot.runner import runner
 
 if __name__ == "__main__":
     logging.basicConfig(
-        filename="log.log", filemode="a", level=logging.INFO,
+        filename="log.log",
+        filemode="a",
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
 
     logger = logging.getLogger("FundingBot")

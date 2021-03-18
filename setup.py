b63b8354c8e3dea97e06c8bd215839f67c1e5708
Kevin Hwa <liverpoo1026.bne@gmail.com>
2021-03-19 00:22:55 +1000

    Version 2.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="funding-bot",
-    version="2.4.1",
+    version="2.5.0",
     description="Handle automatic funding on Bitfinex",
     url="https://github.com/liverpool1026/funding_bot",
     author="Kevin Hwa",
